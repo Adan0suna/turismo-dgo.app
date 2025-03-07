e30b9e8f1cbaa670955bdd725d58647c30c7023f
Birbin <104599486+Link-23@users.noreply.github.com>
2025-03-06 20:08:04 -0600

    Update logica.js
    repo back

--- Backend/logica.js
+++ Backend/logica.js
@@ -1,3 +1,2 @@
-for (let i = 0; i < 5; i++) {
-    console.log("aqui la logica");
-}
+este repositorio valio madres no me dejo subir los cambios para back, asi que me vi en la necesidad de crear otro repositorio para el back 
+https://github.com/Link-23/Arquitectura
